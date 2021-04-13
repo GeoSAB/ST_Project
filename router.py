3915db7661e5d2b784543b1a582c843ca57298b8
zakynator <zakynator@hotmail.it>
2021-04-13 21:26:49 +0200

    updated router

--- router.py
+++ router.py
@@ -4,84 +4,83 @@ from digi.xbee.devices import *
 import hashlib
 import os
 
-
-device = XBeeDevice("/dev/ttyUSB1", 9600)
+hashData=""
+contentGlobal = ""
+device = XBeeDevice("/dev/ttyUSB0", 9600)
 device.open()
 
-def checkForTransmissions():
+def checkForTransmissions(_message = None, _resp= None):
     print("checking trans")
-    Msg = device.read_data()
-    if Msg:
-        content = Msg.data
+    global contentGlobal
+    if _message:
+        content = _message.data
         print("new message TRANS")
         content = content.decode()
         if not content[0:3] == "ms:" :
-            print("ERROR transmission")
             return False
-        print(content[3:])
-        hashedData = hashData(content[3:]).hexdigest()
-        print(hashedData)
+        contentGlobal = content
+        hashedData = MD5hashData(content[3:]).hexdigest()
         try:
-            sendData("hs:"+hashedData,Msg.remote_device)
+            sendData("hs:"+hashedData,_message.remote_device)
+            print("hash sent")
         except:
             print("couldn't send hash to END_DEVICE")
-        response = device.read_data(100000)
-        if not response.remote_device == Msg.remote_device :
-            while not response.remote_device == Msg.remote_device :
-                response = device.read_data(100000)
+            return False
         
-        if response :
-            responseContent = response.data
-            responseContent =responseContent.decode()
-            print("RESP: ",responseContent)
-            if responseContent == "an:ok" :
-                storeData(content)
-                return True
-            else :
-                print("NOPE")
-                return False
+    response = _resp
+    if response :
+        print("AAAAAAAAAAAAAAAAA")
+        responseContent = response.data
+        responseContent =responseContent.decode()
+        print("RESP: ",responseContent)
+        if responseContent == "an:ok" :
+            contentGlobal = ""
+            storeData(contentGlobal)
+            return True
+        else :
+            print("NOPE TRANS")
+            return False
     else : return False
 
-def checkForCoordinator():
-    Msg = device.read_data(1000)
-    if Msg:
-        content = Msg.data
+def checkForCoordinator(_message=None, _resp = None):
+    global hashData
+    if _message:
+        content = _message.data
         print("new message COORD")
         content = content.decode()
-        print(content)
+        print("FSFDSFSF",content)
         if(content == "co:") :
             dataToSend = ""
             file = open("data.txt","r")
             for line in file.readlines() :
                 dataToSend = dataToSend + line
             
-            hashData = hashlib.md5(dataToSend.encode())
+            hashData = hashlib.md5(dataToSend.encode()).hexdigest()
             
-            sendData(dataToSend,Msg.remote_device)
+            try :
+                sendData(dataToSend,Msg.remote_device)
+            except :
+                print("CANT SEND DATA TO COORD")
+                return
 
-            response = device.read_data(100000)
-
-            if response :
-                responseContent = response.data
-                responseContent =responseContent.decode()
-                print("resp coord",responseContent)
-                if responseContent == hashData :
-                    sendData("data_ok", Msg.remote_device)
-                    print("data sent")
-                    os.remove("data.txt")
-                else :
-                    print("NOPE COORD")
+            #response = device.read_data(1000)
+    response = _resp
+    if response :
+        responseContent = response.data
+        responseContent =responseContent.decode()
+        print("resp coord",responseContent, "mine",hashData)
+        
+        if responseContent[0:3] == "hs:" and responseContent[3:] == hashData :
+            sendData("an:ok", Msg.remote_device)
+            hashData = ""
+            print("data sent")
+            os.remove("data.txt")
+        else :
+            print("NOPE COORD")
 
             
-def checkCoordinator():
-    Msg = device.read_data()
-    if Msg:
-        content = Msg.data
-        print("new message")
-        content = content.decode()
-        print(content)
 
-def hashData(data) : 
+def MD5hashData(data) : 
     return hashlib.md5(data.encode())
 
 def sendData(data,remote_device):
@@ -95,15 +94,28 @@ def broadcastPresence() :
         print("Send error")
 
 def storeData(data):
-    file = open("data.txt","w")
+    file = open("data.txt","a")
     file.writelines(data)
     file.close()
 
 while 1 :
-    broadcastPresence()
-    isData = checkForTransmissions()
-    if os.path.exists("data.txt") :
-        checkForCoordinator()
-
+    broadcastPresence()  
+    Msg = device.read_data()
+    if Msg:
+        content = Msg.data
+       
+        content = content.decode()
+        print("new message READ",content)
+        if content[0:3] == "ms:" :
+            checkForTransmissions(_message=Msg)
+        elif content == "an:ok" :
+            checkForTransmissions(_resp=Msg)
+        elif content[0:3] == "co:" :
+            if os.path.exists("data.txt") :
+                checkForCoordinator(_message=Msg)
+                print("")
+        elif content[0:3] == "hs:" :
+            if os.path.exists("data.txt") :
+                checkForCoordinator(_resp=Msg)
     #checkCoordinator()
-    sleep(1)
+   # sleep(0.2)
