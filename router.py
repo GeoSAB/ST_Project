b157f159d7647db83297814dfa942d4e83d5b4c3
zakynator <zakynator@hotmail.it>
2021-04-13 15:06:52 +0200

    router updated

--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-import XBee
+#import XBee
 from time import sleep
 from digi.xbee.devices import *
 import hashlib
@@ -9,18 +9,22 @@ device = XBeeDevice("/dev/ttyUSB1", 9600)
 device.open()
 
 def checkForTransmissions():
-    Msg = device.read_data(100000)
+    print("checking trans")
+    Msg = device.read_data()
     if Msg:
         content = Msg.data
-        print("new message")
+        print("new message TRANS")
         content = content.decode()
-        if not content[:2] == "ms:"
+        if not content[0:3] == "ms:" :
+            print("ERROR transmission")
             return False
-
-        hashedData = hasData(content).hexdigest()
-        
-        sendData(hashedData,Msg.remote_device)
-        
+        print(content[3:])
+        hashedData = hashData(content[3:]).hexdigest()
+        print(hashedData)
+        try:
+            sendData("hs:"+hashedData,Msg.remote_device)
+        except:
+            print("couldn't send hash to END_DEVICE")
         response = device.read_data(100000)
         if not response.remote_device == Msg.remote_device :
             while not response.remote_device == Msg.remote_device :
@@ -29,6 +33,7 @@ def checkForTransmissions():
         if response :
             responseContent = response.data
             responseContent =responseContent.decode()
+            print("RESP: ",responseContent)
             if responseContent == "an:ok" :
                 storeData(content)
                 return True
@@ -38,18 +43,19 @@ def checkForTransmissions():
     else : return False
 
 def checkForCoordinator():
-    Msg = device.read_data(100000)
+    Msg = device.read_data(1000)
     if Msg:
         content = Msg.data
-        print("new message")
+        print("new message COORD")
         content = content.decode()
-        if(content == "coordinator") :
+        print(content)
+        if(content == "co:") :
             dataToSend = ""
-            file = open("file.txt",r)
-            for line in file.readlines :
+            file = open("data.txt","r")
+            for line in file.readlines() :
                 dataToSend = dataToSend + line
             
-            hashData = hashlib.md5(dataToSend)
+            hashData = hashlib.md5(dataToSend.encode())
             
             sendData(dataToSend,Msg.remote_device)
 
@@ -58,32 +64,46 @@ def checkForCoordinator():
             if response :
                 responseContent = response.data
                 responseContent =responseContent.decode()
+                print("resp coord",responseContent)
                 if responseContent == hashData :
                     sendData("data_ok", Msg.remote_device)
                     print("data sent")
                     os.remove("data.txt")
                 else :
-                    print("NOPE")
+                    print("NOPE COORD")
 
             
+def checkCoordinator():
+    Msg = device.read_data()
+    if Msg:
+        content = Msg.data
+        print("new message")
+        content = content.decode()
+        print(content)
 
 def hashData(data) : 
-    return hashlib.md5(data)
+    return hashlib.md5(data.encode())
 
 def sendData(data,remote_device):
     device.send_data(remote_device,data)
 
 def broadcastPresence() :
-    device.send_data_broadcast("router")
+    try :
+        print("router")
+        device.send_data_broadcast("rq:")
+    except:
+        print("Send error")
 
 def storeData(data):
-    file = open("data.txt")
-    file.write(data)
+    file = open("data.txt","w")
+    file.writelines(data)
     file.close()
 
 while 1 :
     broadcastPresence()
     isData = checkForTransmissions()
-    if isData :
+    if os.path.exists("data.txt") :
         checkForCoordinator()
-    sleep(0.5)
+
+    #checkCoordinator()
+    sleep(1)
