835bde14e6c6e2c5d27e3cecdfb9fb5ed2ea953d
Geoffrey <gsabatier64@me.com>
2021-04-13 15:06:14 +0200

    coor-changes

--- coordinator.py
+++ coordinator.py
@@ -8,9 +8,7 @@ data_test = []
 s = "hello this is a message from the router"
 r_hash = hashlib.md5(s.encode())
 
-remote_device = "test"
-router_id = "test"
-coordinator_device = ""
+remote_device =  None
 current_data = "" #store the data while it's being check to see if it's ok
 
 
@@ -27,36 +25,44 @@ def answer(Msg, remote_device):
     print(content)
     #device.send_data(remote_device, content)
 
-def generate_answer(Msg):
+def generate_answer(Msg, coor):
     global remote_device
-    global coordinator_device
-    data_msg = Msg
+    data_msg = (Msg.data).decode()
     remote_device = "test"
     bstr = ""
     hash = ""
 
-    if(remote_device == router_id): #to be addressed later
+    if(remote_device): #to be addressed later
         bstr = data_msg[0:3]
-        print(bstr)
         msg = data_msg[3:]
-        print(msg)
         if(bstr == "ms:"):
-            hash = generate_hash(msg)
-            if(hash.hexdigest() == r_hash.hexdigest()):
-                data_test.append("an:ok")
-            else :
-                data_test.append("an:nk")
-                #coordinator_device.send_data(remote_device, hash)
+            print(data_msg)
+            print("received data")
+            hash = generate_hash(data_msg)
+            try:
+
+                print(Msg.remote_device)
+                print(hash.hexdigest())
+                coor.send_data(Msg.remote_device, hash.hexdigest())
+                print("sent")
+            except:
+                print("Error while sending the hash")
+            current_data = msg
 
         elif (bstr == "an:"):
+            print(bstr)
+            print(msg)
             if(msg == "ok"):
                 k = []
                 now = datetime.now()
                 k.append(now)
                 k.append(current_data)
                 data.append(k)
+                current_data = ""
             else :
                 print("error the data hasn't been received correctly")
+        elif (bstr == "rq:"):
+            pass
         else:
             print("unrecognized msg")
     else:
@@ -64,24 +70,27 @@ def generate_answer(Msg):
 
 
 def main():
-    #device = XBeeDevice("/dev/tty.usbserial-A50285BI", 9600)
-    #device.open()
 
-    router_msg = "ms:hello this is a message from the router"
-    data_test.append(router_msg)
-    i = 0
-    while(i < len(data_test)):
+    coordinator_device = XBeeDevice("/dev/tty.usbserial-A50285BI", 9600)
+    coordinator_device.open()
+    router_msg = None
+
+    while(True):
         print("ready")
-
-        #router_msg = device.read_data(100000)
+        try:
+            coordinator_device.send_data_broadcast("co:")
+        except:
+            d = datetime.now()
+            print("could not broadcast " + d.strftime("%m/%d/%Y, %H:%M:%S"))
+        
+        try:
+            router_msg = coordinator_device.read_data()
+        except:
+            d = datetime.now()
+            print("could not read data " + d.strftime("%m/%d/%Y, %H:%M:%S"))
 
         if router_msg:
-            print("got a message")
-            generate_answer(data_test[i])
-
-            sleep(2)
-
-        i += 1
+            generate_answer(router_msg, coordinator_device)
             
 
         # Send broadcast data.
