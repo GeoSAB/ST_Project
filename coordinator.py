0e7703096802e18442b740c73af31a8694ee8710
Geoffrey <gsabatier64@me.com>
2021-04-15 15:22:17 +0200

    t

--- coordinator.py
+++ coordinator.py
@@ -4,13 +4,13 @@ from time import sleep
 from pathlib import Path
 from digi.xbee.devices import *
 
+message = ""
 data = []
 data_test = []
 
 remote_device =  None
 current_data = "" #store the data while it's being check to see if it's ok
 
-c = False
 
 
 #def receive_data(rm_device):
@@ -26,11 +26,10 @@ def answer(Msg, remote_device):
     #device.send_data(remote_device, content)
 
 def generate_answer(Msg, coor):
-    global c
+    global message
     global remote_device
     data_msg = (Msg.data).decode()
     remote_device = "test"
-    router_ans = None
     bstr = ""
     hash = ""
 
@@ -46,35 +45,27 @@ def generate_answer(Msg, coor):
                 print(Msg.remote_device)
                 print(hash.hexdigest())
                 st = "hs:"
-                st += str(hash.hexdigest())
+                st += hash.hexdigest()
                 print(st)
                 coor.send_data(Msg.remote_device, st)
-                data.append(msg)
-                print("sent")
+                print("hash sent")
+                message = msg
             except:
                 print("Error while sending the hash")
-            try:
-                router_ans = coor.read_data(100)
-                data_msg = (Msg.data).decode()
-                bstr = data_msg[0:3]
-                msg = data_msg[3:]
-                if (bstr == "an:"):
-                    print(bstr)
-                    print(msg)
-                    if(msg == "ok"):
-                        now = datetime.now()
-                        f = open("data.txt", "a")
-                        f.write(now.strftime("%m/%d/%Y, %H:%M:%S") + " " + data[-1])
-                    else :
-                        print("error the data hasn't been received correctly")
-                        data.remove(-1)
-            
-            except:
-                d = datetime.now()
-                print("could not read data " + d.strftime("%m/%d/%Y, %H:%M:%S"))
 
+        elif (bstr == "an:"):
+            print(bstr)
+            print(msg)
+            if(msg == "ok" and message):
+                now = datetime.now()
+                f = open("coor.txt", "a")
+                f.write(now.strftime("%m/%d/%Y, %H:%M:%S") + " | " + message + "\n")
+                message = ""
+            else :
+                print("error the data hasn't been received correctly")
         elif (bstr == "rq:"):
             pass
+
         else:
             print("unrecognized msg")
     else:
@@ -88,9 +79,10 @@ def main():
     router_msg = None
 
     while(True):
-        print("ready")
+
         try:
-                coordinator_device.send_data_broadcast("co:")
+            coordinator_device.send_data_broadcast("co:")
+            print("Broadcast")
         except:
             d = datetime.now()
             print("could not broadcast " + d.strftime("%m/%d/%Y, %H:%M:%S"))
