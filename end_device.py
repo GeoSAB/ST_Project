c1ebe7deccaac4e12261c9402ebc9d5c42e9c264
zakynator <zakynator@hotmail.it>
2021-04-16 10:58:29 +0200

    updated enddevice to use class list for hash check

--- end_device.py
+++ end_device.py
@@ -1,15 +1,34 @@
-import XBee
+#import XBee
 from time import sleep
 from digi.xbee.devices import *
 import hashlib
+
+class MessageCheck:
+    def __init__(self,message,hashDigest):
+        self.message = message
+        self.hashDigest = hashDigest
+
+
+
+
+messageList = []
 
 data_to_send = ""
 data_hash = ""
 num_of_line = 0
 drone_id = "0013A20041C60D23"
 
+def isHashInSentList(hs):
+    global messageList
+    for message in messageList :
+        if (message.hashDigest == hs):
+            messageList.remove(message)
+            return True
+    return False
+    
+
 def read_sensor_data(line_num):
-    with open("data.txt") as file:
+    with open("read.txt") as file:
         content = file.readlines()
     return content[num_of_line]
 
@@ -25,7 +44,7 @@ def generate_answer(Msg):
 
     if (str(remote_device)[0:16] == drone_id):
 
-        print("success id")
+        #print("success id")
         #treat message
 
         if (code == "rq"):
@@ -35,30 +54,29 @@ def generate_answer(Msg):
             data_hash = hashlib.md5(data_to_send.encode()).hexdigest()
             data_to_send = "ms:" + data_to_send
 
-            print("sending data: ", data_to_send)
+            print("sending data to router: ", data_to_send)
 
-            return remote_device, data_to_send
+            return remote_device, data_to_send , data_hash
 
         elif (code == "hs"):
-            print("received hash:", data[3:], data_hash)
-            if (data[3:] == data_hash):
+            print("received hash from router:", data[3:], data_hash)
+            if (isHashInSentList(data[3:])):
                 num_of_line += 1
-                return remote_device, "an:ok"
+                return remote_device, "an:ok",data_hash
 
             else:
-                if (num_of_line != 0):
-                    num_of_line -= 1
-                return remote_device, "an:nk"
+              
+                return remote_device, "an:nk",data_hash
         else:
-            remote_device, "an:unknown"
+            remote_device, "an:unknown", 0
 
     else:
-        return 0, 0
+        return 0, 0, 0
 
 
 
 def main():
-
+    global messageList
     device = XBeeDevice("/dev/ttyUSB0", 9600)
     device.open()
 
@@ -69,12 +87,13 @@ def main():
         print("message ", (msg.data).decode())
 
         if msg:    
-            remote_device, data = generate_answer(msg)
+            remote_device, data ,hash_data = generate_answer(msg)
             if (remote_device != 0):
+                m = MessageCheck(data,hash_data)
+                messageList.append(m)
                 device.send_data(remote_device, data)
             print(remote_device, data)
 
 
 if __name__ == "__main__":
     main()
-
